98c88a45aeab9e5daa5a2e950049e584034f2e64
jaouad.ifatssane <jaouad.ifatssane@gmail.com>
2022-03-21 10:07:04 +0100

    modify terraform to make unify RGs

--- scripts/infrastructure/backend.tf
+++ scripts/infrastructure/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "jaouad-aks-dockercoins-storage"
+    resource_group_name  = "jaouad-aks-dockercoins"
     storage_account_name = "jaouadterraformstorage"
     container_name       = "jaouad-terraform-container"
     key                  = "terraform.tfstate"
--- scripts/infrastructure/terraform.tfvars
+++ scripts/infrastructure/terraform.tfvars
@@ -3,4 +3,4 @@ location            = "westeurope"
 cluster_name        = "dockercoins-cluster"
 kubernetes_version  = "1.23.3"
 system_node_count   = 2
-acr_name            = "jaouadacr"
+acr_name            = "dockercoins-acr"
