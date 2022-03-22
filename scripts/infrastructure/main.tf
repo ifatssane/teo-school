d6dc86dbf936ff59912d668005d5e4635317818b
jaouad.ifatssane <jaouad.ifatssane@gmail.com>
2022-03-22 14:51:12 +0100

    modify k8s config deployment

--- scripts/infrastructure/main.tf
+++ scripts/infrastructure/main.tf
@@ -9,7 +9,7 @@
 resource "azurerm_role_assignment" "role_acrpull" {
   scope                            = azurerm_container_registry.acr.id
   role_definition_name             = "AcrPull"
-  principal_id                     = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
   skip_service_principal_aad_check = true
 }
 
@@ -17,7 +17,7 @@ resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  sku                 = "Standard"
+  sku                 = "Basic"
   admin_enabled       = false
 }
 
@@ -34,7 +34,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     vm_size             = "Standard_DS2_v2"
     type                = "VirtualMachineScaleSets"
     availability_zones  = [1, 2, 3]
-    enable_auto_scaling = false
+    enable_auto_scaling = true
   }
 
   identity {
