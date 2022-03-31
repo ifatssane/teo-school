7f1751465b56c9cab905946627f6c0432e52c9e7
jaouad.ifatssane <jaouad.ifatssane@gmail.com>
2022-03-31 18:20:27 +0200

    add prometheus stack infra

--- scripts/infrastructure/providers.tf
+++ scripts/infrastructure/providers.tf
@@ -2,11 +2,25 @@ provider "azurerm" {
   features {}
 }
 
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.39.0"
+      version = "3.0.2"
     }
+/*      helm = {
+       source  = "hashicorp/helm"
+       version = "~> 3.0.0"
+     }  */
   }
 }
