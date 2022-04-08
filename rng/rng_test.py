90185daf43fce9972e3f364f9ea3b3f3ab0b082b
jaouad.ifatssane <jaouad.ifatssane@gmail.com>
2022-04-08 14:44:46 +0200

    add rng test file

---
+++ rng/rng_test.py
@@ -0,0 +1,15 @@
+import rng
+import socket
+import pytest
+
+@pytest.fixture 
+def index_test():
+    return rng.index()
+
+def test_index_content(index_test):
+    hostname = socket.gethostname()
+    assert  "RNG running on {}\n".format(hostname) in index_test 
+
+def test_rng_status():
+    statuscode = rng.rng(32).status_code
+    assert statuscode == 200
