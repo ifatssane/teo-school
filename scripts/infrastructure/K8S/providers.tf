ec7d9102f14177eaf7d16d1d8d487fe9e8e4d615
jaouad.ifatssane <jaouad.ifatssane@gmail.com>
2022-04-22 14:29:13 +0200

    refactor

--- scripts/infrastructure/K8S/providers.tf
+++ scripts/infrastructure/K8S/providers.tf
@@ -4,12 +4,12 @@ provider "azurerm" {
 
 provider "helm" {
   kubernetes {
-    config_path = ".kube/config"
+    config_path = "~/.kube/config"
   }
 }
 
 provider "kubernetes" {
-  config_path = ".kube/config"
+  config_path = "~/.kube/config"
 }
 
 terraform {
