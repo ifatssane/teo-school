3ba4e144b815bff7f26613383fb56223c7b6df7c
jaouad.ifatssane <jaouad.ifatssane@gmail.com>
2022-04-22 16:16:27 +0200

    refactor

--- scripts/infrastructure/app-services/main.tf
+++ scripts/infrastructure/app-services/main.tf
@@ -201,12 +201,6 @@ resource "azurerm_function_app" "hasher-function-app" {
   storage_account_name       = var.storage_account_name
   storage_account_access_key = var.storage_account_access_key
 
-
-  lifecycle {
-    ignore_changes = [
-      site_config["linux_fx_version"]
-    ]
-  }
   depends_on = [
     azurerm_container_registry.acr
   ]
